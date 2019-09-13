00b96b8e5d6051d30f248c6bc8b7dce082634850
christophe.poty <christophe.poty@hotmail.com>
2019-09-13 11:26:23 +0200

    Improve the bus

---
+++ MicroRabbit/MicroRabbit.Transfer.Data/Migrations/20190912231240_ChangeColumnNameTransferAmount.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MicroRabbit.Transfer.Data.Migrations
+{
+    public partial class ChangeColumnNameTransferAmount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Transferamount",
+                table: "TransferLogs",
+                newName: "TransferAmount");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TransferAmount",
+                table: "TransferLogs",
+                newName: "Transferamount");
+        }
+    }
+}
